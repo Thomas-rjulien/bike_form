24add5c32cfff0afbda2cea8be5b4b8acd66b586
thomas julien <thomas.julien@skema.edu>
2020-02-08 15:55:56 +0100

    testing

--- bike4balance/app/controllers/booking_controller.rb
+++ bike4balance/app/controllers/booking_controller.rb
@@ -4,9 +4,16 @@ class BookingController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(set_booking_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def index
+    @bookings = Booking.all
   end
 
   def show
@@ -17,9 +24,8 @@ class BookingController < ApplicationController
 
   private
 
-  booking_parameters = %i[name email comment heights checkin checkout baby_chair spd_pedals insurance hitch_bike_racks trunk_bike_rack]
-
   def set_booking_params
+    booking_parameters = %i[name email comment heights checkin checkout baby_chair spd_pedals insurance hitch_bike_racks trunk_bike_rack]
     params.require(:booking).permit(booking_parameters)
   end
 end
